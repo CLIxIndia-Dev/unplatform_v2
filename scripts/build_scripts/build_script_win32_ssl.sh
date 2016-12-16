de6a7895f50b45814d1c77415d14ee04b1d0621c
cjshaw <cjshaw@mit.edu>
2016-12-16 15:04:07 -0500

    update windows script

--- scripts/build_scripts/build_script_win32_ssl.sh
+++ scripts/build_scripts/build_script_win32_ssl.sh
@@ -11,29 +11,28 @@ git pull
 pip install -r requirements.txt
 
 rm -rf bundle/
+rm -r build/
 
 WEBENV=test nosetests tests
 
 npm run compile:ui
 pyinstaller main.spec
 
-rm -r build
-
-mv dist/main bundle
-cp -r static/ bundle
+mv dist/main bundle/unplatform.exe
+cp -r static/ bundle/
 
 cp unplatform/unplatform.cert.dummy.pem bundle/unplatform/
 cp unplatform/unplatform.key.dummy.pem bundle/unplatform/
 
 cp scripts/launchers/unplatform_win32_ssl.bat bundle/
 
-FILE=$(find unplatform_distributable/qbank/ -name qbank-lite*.exe | sort -n | tail -1)
+FILE=$(find external_packages/qbank/ -name qbank-lite*.exe | sort -n | tail -1)
 cp $FILE bundle/
 
 cp scripts/data_extraction_scripts/DataExtractionScript.bat bundle/
 cp scripts/data_extraction_scripts/zipjs.bat bundle/
 
-cp -r README bundle/
+cp README bundle/
 
 VERSION=$(awk -F": " '$1=="version"{print $2}' package.json)
 zip -r "unplatform_v${VERSION//\'/}_win32.zip" bundle
