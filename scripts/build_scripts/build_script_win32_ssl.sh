3036f162bfd7aef1b91d98d18027c3758fd88814
cjshaw <cjshaw@mit.edu>
2016-12-16 15:30:15 -0500

    clean up script

--- scripts/build_scripts/build_script_win32_ssl.sh
+++ scripts/build_scripts/build_script_win32_ssl.sh
@@ -18,7 +18,9 @@ WEBENV=test nosetests tests
 pyinstaller main.spec
 
 mv dist\main.exe bundle\unplatform.exe
-cp -r static\ bundle\
+
+mkdir bundle\static
+cp -r static\ bundle\static\
 
 mkdir bundle\unplatform
 cp unplatform\unplatform.cert.dummy.pem bundle\unplatform\
@@ -27,7 +29,7 @@ cp unplatform\unplatform.key.dummy.pem bundle\unplatform\
 cp scripts\launchers\unplatform_win32_ssl.bat bundle\
 
 FILE=$(find external_packages\qbank\ -name qbank-lite*.exe | sort -n | tail -1)
-cp $FILE bundle\
+cp $FILE bundle\$FILE
 
 cp scripts\data_extraction_scripts\DataExtractionScript.bat bundle\
 cp scripts\data_extraction_scripts\zipjs.bat bundle\
@@ -35,4 +37,7 @@ cp scripts\data_extraction_scripts\zipjs.bat bundle\
 cp README bundle\
 
 VERSION=$(python -c "import json; print json.load(open('package.json', 'rb'))['version']")
-zip -r "unplatform_v${VERSION//\'/}_win32.zip" bundle
+OUTPUT="unplatform_v${VERSION//\'/}_win32.zip"
+zip -r $OUTPUT bundle
+
+mv $OUTPUT bundle\$OUTPUT
