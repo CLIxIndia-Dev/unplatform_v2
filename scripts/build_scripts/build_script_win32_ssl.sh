dc8cf837962cbe3692ccf22d3be99721268a397c
cjshaw <cjshaw@mit.edu>
2016-12-16 15:18:16 -0500

    change to forward slashes

--- scripts/build_scripts/build_script_win32_ssl.sh
+++ scripts/build_scripts/build_script_win32_ssl.sh
@@ -10,29 +10,29 @@ git pull
 
 pip install -r requirements.txt
 
-rm -rf bundle/
-rm -r build/
+rm -rf bundle\
+rm -r build\
 
 WEBENV=test nosetests tests
 
 pyinstaller main.spec
 
-mv dist/main.exe bundle/unplatform.exe
-cp -r static/ bundle/
+mv dist\main.exe bundle\unplatform.exe
+cp -r static\ bundle\
 
-mkdir bundle/unplatform
-cp unplatform/unplatform.cert.dummy.pem bundle/unplatform/
-cp unplatform/unplatform.key.dummy.pem bundle/unplatform/
+mkdir bundle\unplatform
+cp unplatform\unplatform.cert.dummy.pem bundle\unplatform\
+cp unplatform\unplatform.key.dummy.pem bundle\unplatform\
 
-cp scripts/launchers/unplatform_win32_ssl.bat bundle/
+cp scripts\launchers\unplatform_win32_ssl.bat bundle\
 
-FILE=$(find external_packages/qbank/ -name qbank-lite*.exe | sort -n | tail -1)
-cp $FILE bundle/
+FILE=$(find external_packages\qbank\ -name qbank-lite*.exe | sort -n | tail -1)
+cp $FILE bundle\
 
-cp scripts/data_extraction_scripts/DataExtractionScript.bat bundle/
-cp scripts/data_extraction_scripts/zipjs.bat bundle/
+cp scripts\data_extraction_scripts\DataExtractionScript.bat bundle\
+cp scripts\data_extraction_scripts\zipjs.bat bundle\
 
-cp README bundle/
+cp README bundle\
 
 VERSION=$(python -c "import json; print json.load(open('package.json', 'rb'))['version']")
 zip -r "unplatform_v${VERSION//\'/}_win32.zip" bundle
