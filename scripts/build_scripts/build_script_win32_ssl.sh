bb6c3184f6d75ccfeb03e35eee68ed745cb7a55b
cjshaw <cjshaw@mit.edu>
2016-12-19 09:49:04 -0500

    use git clean instead of manually removing build directories and artifacts

--- scripts/build_scripts/build_script_win32_ssl.sh
+++ scripts/build_scripts/build_script_win32_ssl.sh
@@ -2,42 +2,33 @@
 
 cd ../../
 
-find . -name "*.pyc" -type f -delete
-find . -name ".DS_Store" -type f -delete
-find . -name ".directory" -type f -delete
-
-git pull
-
-pip install -r requirements.txt
-
-rm -rf bundle\
-rm -r build\
+git clean -x -d
 
 WEBENV=test nosetests tests
 
 pyinstaller main.spec
 
-mv dist\main.exe bundle\unplatform.exe
+mv dist/main.exe bundle/unplatform.exe
 
-mkdir bundle\static
-cp -r static\ bundle\static\
+mkdir bundle/static
+cp -r static/ bundle/static/
 
-mkdir bundle\unplatform
-cp unplatform\unplatform.cert.dummy.pem bundle\unplatform\
-cp unplatform\unplatform.key.dummy.pem bundle\unplatform\
+mkdir bundle/unplatform
+cp unplatform/unplatform.cert.dummy.pem bundle/unplatform/
+cp unplatform/unplatform.key.dummy.pem bundle/unplatform/
 
-cp scripts\launchers\unplatform_win32_ssl.bat bundle\
+cp scripts/launchers/unplatform_win32_ssl.bat bundle/
 
-FILE=$(find external_packages\qbank\ -name qbank-lite*.exe | sort -n | tail -1)
-cp $FILE bundle\$FILE
+FILE=$(find external_packages/qbank/ -name qbank-lite*.exe | sort -n | tail -1)
+cp $FILE bundle/$FILE
 
-cp scripts\data_extraction_scripts\DataExtractionScript.bat bundle\
-cp scripts\data_extraction_scripts\zipjs.bat bundle\
+cp scripts/data_extraction_scripts/DataExtractionScript.bat bundle/
+cp scripts/data_extraction_scripts/zipjs.bat bundle/
 
-cp README bundle\
+cp README bundle/
 
 VERSION=$(python -c "import json; print json.load(open('package.json', 'rb'))['version']")
 OUTPUT="unplatform_v${VERSION//\'/}_win32.zip"
 zip -r $OUTPUT bundle
 
-mv $OUTPUT bundle\$OUTPUT
+mv $OUTPUT bundle/$OUTPUT
