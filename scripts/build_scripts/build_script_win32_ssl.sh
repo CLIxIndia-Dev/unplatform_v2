65917acf958acbf32d149e0bdbb1e6b858671851
cjshaw <cjshaw@mit.edu>
2017-03-21 13:08:21 -0400

    copy the template files in the windows build script

--- scripts/build_scripts/build_script_win32_ssl.sh
+++ scripts/build_scripts/build_script_win32_ssl.sh
@@ -22,6 +22,10 @@ rm -rf static/ui
 mkdir bundle/
 mkdir bundle/static/
 
+# copy the session expired template
+mkdir bundle/templates/
+cp templates/ bundle/templates/
+
 # update the virtualenvironment
 pip install -r requirements.txt
 
