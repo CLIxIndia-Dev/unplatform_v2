8f428c4ef115ce95a2ff0b29bf48c88dfb3cd018
cjshaw <cjshaw@mit.edu>
2017-04-03 10:02:50 -0400

    better streaming algorithm that accepts byte range

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import mimetypes
 import urllib
 import requests
 import functools
+import codecs
 
 from datetime import datetime
 from natsort import natsorted
@@ -232,15 +233,59 @@ class content:
             url = urllib.pathname2url(full_path)
             mimetype = mimetypes.guess_type(url)
             web.header('Content-Type', mimetype[0])
-            web.header('Content-Length', os.path.getsize(full_path))
+            # web.header('Content-Length', os.path.getsize(full_path))
             web.header('Accept-Ranges', 'bytes')
-            with open(full_path, 'rb') as content_file:
-                num_bytes_to_read = 1024 * 8
-                while 1:
-                    buf = content_file.read(num_bytes_to_read)
-                    if not buf:
-                        break
-                    yield buf
+
+            file_handle = codecs.open(full_path, 'r', encoding='utf-8')
+            try:
+                file_handle.read()
+            except UnicodeDecodeError:
+                file_handle = open(full_path, 'rb')
+            file_handle.seek(0)
+
+            # The algorithm below for streaming partial content was based off of this
+            # post:
+            # https://benramsey.com/blog/2008/05/206-partial-content-and-range-requests/
+
+            continue_with_stream = True
+            byte_range = utilities.get_byte_ranges()
+            total_bytes_to_read = os.path.getsize(file_handle.name)
+            content_length = os.path.getsize(file_handle.name)
+            bytes_to_throw_away = 0
+            if byte_range is not None:
+                bytes_to_throw_away = int(byte_range[0])
+                if bytes_to_throw_away > total_bytes_to_read or bytes_to_throw_away < 0:
+                    web.ctx.status = '416 Requested Range Not Satisfiable'
+                    continue_with_stream = False
+                    yield ''
+                file_handle.read(bytes_to_throw_away)
+                total_bytes_to_read = os.path.getsize(file_handle.name) - bytes_to_throw_away
+                if byte_range[1] != '':
+                    total_bytes_to_read = int(byte_range[1]) - bytes_to_throw_away
+
+            bytes_read = 0
+
+            num_bytes_to_read = 1024 * 8
+            starting_bytes = bytes_to_throw_away
+            web.ctx.status = '206 Partial Content'
+
+            while continue_with_stream:
+                remaining_bytes = total_bytes_to_read - bytes_read
+                bytes_to_read = min(num_bytes_to_read, remaining_bytes)
+                buf = file_handle.read(bytes_to_read)
+                if not buf:
+                    break
+
+                # web.header('Content-Length', str(bytes_to_read))
+                web.header('Content-Range', 'bytes {0}-{1}/{2}'.format(str(starting_bytes),
+                                                                       str(starting_bytes + bytes_to_read),
+                                                                       str(content_length)))
+
+                bytes_read += bytes_to_read
+                starting_bytes += bytes_to_read
+                yield buf
+
+            file_handle.close()
 
 
 class modules_list:
