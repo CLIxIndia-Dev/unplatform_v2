a13b5620e552979aa31e8735ee285e6dc541d0bb
resource11 <mcmahon@resource11.com>
2017-08-20 22:47:14 -0400

    removes Radium from Activity component

--- ui/src/routes/Activities/Activities.js
+++ ui/src/routes/Activities/Activities.js
@@ -1,74 +1,14 @@
-import _ from 'lodash'
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { browserHistory } from 'react-router'
-import Radium from 'radium'
-import {Icon} from 'react-fa'
-
-import {log} from '../../utilities'
+import { log } from '../../utilities'
 
 import '../../styles/core.css'
 import '../../styles/buttons.css'
 import '../../styles/header.css'
 import '../../styles/activity.css'
-import '../../styles/nav.css'
 
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    // height: '100vh',
-    flexDirection: 'column'
-  },
-  navWrapper: {
-    flex: 1
-  },
-  section: {
-    flex: 50,
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  label: {
-    fontWeight: 'bold',
-    marginRight: '15px',
-    marginBottom: '15px',
-    flex: 2
-  },
-  formRow: {
-    display: 'flex',
-    width: '400px',
-    margin: '5px 5px'
-  },
-  value: {
-    flex: 2
-  },
-  navTitle: {
-  },
-  background: {
-    backgroundColor: 'white'
-  },
-  header: {
-    position: 'relative',
-    left: 0,
-    top: 0,
-    width: '100%',
-    height: '55px',
-    display: 'flex',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.25)',
-    zIndex: 5
-  },
-  nav: {
-    position: 'absolute',
-    top: '29px',
-    right: '1em'
-  }
-}
-
-@Radium
 class Subjects extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       showModal: false
@@ -81,20 +21,20 @@ class Subjects extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps (nextProps) {
 
   }
 
   renderTool = (tool, index) => {
-    return <button className="choice-select"
+    return <button className='choice-select'
       onClick={() => this._onHandleSelectTool(tool)}>{tool}</button>
   }
 
-  render() {
+  render () {
     if (!this.props.locale) {
       return (
         <div>
-          <h1>Please set your school configuration at this <a href="/school">link</a>.</h1>
+          <h1>Please set your school configuration at this <a href='/school'>link</a>.</h1>
         </div>
       )
     }
@@ -103,32 +43,45 @@ class Subjects extends Component {
 
     if (this.state.showModal) {
       sessionModal = (
-        <div className="container-popup">
-          <dialog open className="zoomModal span_6_of_12">
-            <h2 className="zoom-label">{this.props.strings.unplatformNav.endSession}"</h2>
-           <form method="dialog">
-              <button value="close" onClick={this._onFinishLesson}>{this.props.strings.prompt.yes}</button>
-              <button value="close" onClick={this._onToggleModal}>{this.props.strings.prompt.no}</button>
+        <div className='c-act-popup'>
+          <dialog open className='zoomModal span_6_of_12'>
+            <h2 className='zoom-title'>
+              {this.props.strings.unplatformNav.endSession}
+            </h2>
+            <form method='dialog'>
+              <button value='close' onClick={this._onFinishLesson}>
+                {this.props.strings.prompt.yes}
+              </button>
+              <button value='close' onClick={this._onToggleModal}>
+                {this.props.strings.prompt.no}
+              </button>
             </form>
           </dialog>
         </div>
       )
     }
 
-    let epubUrl = `/static/content_player/index.html?epubUrl=/content/${this.props.subjectName}/${this.props.gradeName}/${this.props.unitName}/${this.props.lessonName}`
+    let epubUrl = `/static/content_player/index.html?epubUrl=/content/
+      ${this.props.subjectName}/
+      ${this.props.gradeName}/
+      ${this.props.unitName}/
+      ${this.props.lessonName}`
     return (
-      <div style={styles.container} >
-        <header style={styles.header}>
-          <h1 className="logo">CLIx Connected Learning Initiative</h1>
-          <p className="unplat-v">unplatform version {this.props.version}</p>
-          <nav style={styles.nav}>
-            <button onClick={this._onChooseTool}>{this.props.strings.unplatformNav.chooseTool}</button>
-            <button onClick={this._onChooseLesson}>{this.props.strings.unplatformNav.chooseNewLesson}</button>
-            <button onClick={this._onToggleModal}>{this.props.strings.unplatformNav.finishLesson}</button>
+      <div className='c-container'>
+        <header className='c-header'>
+          <h1 className='logo'>CLIx Connected Learning Initiative</h1>
+          <p className='unplat-v'>unplatform version {this.props.version}</p>
+          <nav className='c-nav'>
+            <button onClick={this._onChooseTool}>
+              {this.props.strings.unplatformNav.chooseTool}</button>
+            <button onClick={this._onChooseLesson}>
+              {this.props.strings.unplatformNav.chooseNewLesson}</button>
+            <button onClick={this._onToggleModal}>
+              {this.props.strings.unplatformNav.finishLesson}</button>
           </nav>
         </header>
-        <main className="span_12_of_12" style={styles.background}>
-          <iframe src={epubUrl} className="fill-win" frameBorder="0" allowfullscreen></iframe>
+        <main className='span_12_of_12 c-background'>
+          <iframe src={epubUrl} className='fill-win' frameBorder='0' allowFullScreen />
           {sessionModal}
         </main>
       </div>
@@ -139,10 +92,13 @@ class Subjects extends Component {
     e.preventDefault()
     log({
       sessionId: this.props.sessionId,
-      appName: "unplatform",
+      appName: 'unplatform',
       action: 'clicked_finished',
       params: {
-        url: `/subjects/${this.props.subjectName}/grades/${this.props.gradeName}/units/${this.props.unitName}/lessons/${this.props.lessonName}`,
+        url: `/subjects/${this.props.subjectName}
+        /grades/${this.props.gradeName}
+        /units/${this.props.unitName}
+        /lessons/${this.props.lessonName}`,
         response: 'yes'
       }
     })
@@ -154,10 +110,13 @@ class Subjects extends Component {
     e.preventDefault()
     log({
       sessionId: this.props.sessionId,
-      appName: "unplatform",
+      appName: 'unplatform',
       action: 'clicked_choose_tool',
       params: {
-        url: `/subjects/${this.props.subjectName}/grades/${this.props.gradeName}/units/${this.props.unitName}/lessons/${this.props.lessonName}`
+        url: `/subjects/${this.props.subjectName}
+        /grades/${this.props.gradeName}
+        /units/${this.props.unitName}
+        /lessons/${this.props.lessonName}`
       }
     })
     browserHistory.push('/tools')
@@ -167,13 +126,19 @@ class Subjects extends Component {
     e.preventDefault()
     log({
       sessionId: this.props.sessionId,
-      appName: "unplatform",
+      appName: 'unplatform',
       action: 'clicked_choose_new_lesson',
       params: {
-        url: `/subjects/${this.props.subjectName}/grades/${this.props.gradeName}/units/${this.props.unitName}/lessons/${this.props.lessonName}`
+        url: `/subjects/${this.props.subjectName}
+        /grades/${this.props.gradeName}
+        /units/${this.props.unitName}
+        /lessons/${this.props.lessonName}`
       }
     })
-    browserHistory.push(`/subjects/${this.props.subjectName}/grades/${this.props.gradeName}/units/${this.props.unitName}`)
+    browserHistory.push(
+      `/subjects/${this.props.subjectName}
+      /grades/${this.props.gradeName}
+      /units/${this.props.unitName}`)
   }
 
   _onToggleModal = (e) => {
