2cf383773fa5ac4dbb8de936ec96c86e05519e99
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2017-12-06 11:26:50 -0500

    for osx, let each app run in a terminal window, so easier to stop

--- scripts/launchers/unplatform_osx_ssl.sh
+++ scripts/launchers/unplatform_osx_ssl.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-./unplatform_osx_ssl &
-sleep 6
+UNPLATFORM_SCRIPT=`pwd`/unplatform_osx_ssl
+echo Running unplatform from $UNPLATFORM_SCRIPT
+osascript -e "tell app \"Terminal\" to do script \"$UNPLATFORM_SCRIPT\""
+sleep 3
 
 FILE=$(find . -name qbank-lite* | sort -n | tail -1)
-./$FILE &
+QBANK_SCRIPT=`pwd`/$FILE
+echo Running qbank from $QBANK_SCRIPT
+osascript -e "tell app \"Terminal\" to do script \"$QBANK_SCRIPT\""
 /usr/bin/open -a "/Applications/Google Chrome.app" 'https://localhost:8888/'
