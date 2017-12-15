c2e13a971f14b956bab1f5a8cfcc53089feb009a
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2017-12-15 12:11:06 -0500

    open new terminal windows for unplatform and qbank

--- scripts/launchers/unplatform_linux64_ssl.sh
+++ scripts/launchers/unplatform_linux64_ssl.sh
@@ -1,5 +1,5 @@
-./unplatform_linux64_ssl &
+x-terminal-emulator -e `pwd`/unplatform_linux64_ssl
 sleep 6
 FILE=$(find . -name qbank-lite*ubuntu* | sort -n | tail -1)
-./$FILE
+x-terminal-emulator -e `pwd`/$FILE
 google-chrome https://localhost:8888
