56ccce0f7cb7c0630bf27471e142e733c816d948
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2017-12-18 12:15:32 -0500

    use full paths for linux builds

--- scripts/launchers/unplatform_linux64_ssl.sh
+++ scripts/launchers/unplatform_linux64_ssl.sh
@@ -1,5 +1,8 @@
-x-terminal-emulator -e `pwd`/unplatform_linux64_ssl
+#!/bin/bash
+# hardcode the paths since we know the .deb
+#   package installs to /opt/clix.
+x-terminal-emulator -e /opt/clix/unplatform_linux64_ssl
 sleep 6
 FILE=$(find . -name qbank-lite*ubuntu* | sort -n | tail -1)
-x-terminal-emulator -e `pwd`/$FILE
+x-terminal-emulator -e /opt/clix/$FILE
 google-chrome https://localhost:8888
