cdc75f19a16dbdef6ab90dad767c33027ab87a90
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2018-02-22 12:18:15 -0500

    change to dev qbank

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-QBANK_LOGGING_ENDPOINT = 'https://localhost:8080/api/v1/logging/logs'
+QBANK_LOGGING_ENDPOINT = 'https://unplatform-dev.mit.edu:8080/api/v1/logging/logs'
 DEFAULT_LOG_GENUS_TYPE = 'log-genus-type%3Adefault-clix%40ODL.MIT.EDU'
