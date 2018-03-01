6a0bc3366e7b93411f37c6eb1c1012753ebaa4bd
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2018-03-01 06:36:37 -0500

    truncate and ellipsize button text

--- ui/src/selectors/index.js
+++ ui/src/selectors/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash'
 
+const MAX_CHARS = 40;
+
 export function getSubjects (state, props) {
   if (!state.module.modules) {
     return null
   }
 
-  return _.filter(_.keys(state.module.modules), (key) => {
-    return key !== 'Tools'})
+  return _.map(_.filter(_.keys(state.module.modules), (key) => {
+    return key !== 'Tools'}),
+    subject => _.truncate(subject, MAX_CHARS))
 }
 
 export function getTools (state, props) {
@@ -30,7 +33,8 @@ export function getGrades (state, props) {
     return null
   }
 
-  return _.keys(state.module.modules[props.params.subjectName])
+  return _.map(_.keys(state.module.modules[props.params.subjectName]),
+    grades => _.truncate(grades, MAX_CHARS))
 }
 
 export function getUnits (state, props) {
@@ -39,7 +43,8 @@ export function getUnits (state, props) {
     return null
   }
 
-  return _.keys(state.module.modules[props.params.subjectName])
+  return _.map(_.keys(state.module.modules[props.params.subjectName]),
+    unit => _.truncate(unit, MAX_CHARS))
 }
 
 export function getLessons (state, props) {
@@ -49,5 +54,6 @@ export function getLessons (state, props) {
     return null
   }
 
-  return _.keys(state.module.modules[props.params.subjectName][props.params.unitName])
+  return _.map(_.keys(state.module.modules[props.params.subjectName][props.params.unitName]),
+    lesson => _.truncate(lesson, MAX_CHARS))
 }
