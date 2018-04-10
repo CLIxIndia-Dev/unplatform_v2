55cb0e1359bd091d854f9ea5f74b007c82c53261
cjshawMIT <4930129+cjshawMIT@users.noreply.github.com>
2018-04-10 13:53:57 -0400

    Feature/cs/update sln apis (#224)
    * change SLN read_only to is_locked and update functionality
    
    * update sln directory name
    
    * try to improve performance of get projects
    
    * add sln lite to build script
    
    * add route for creating locked star logo nova models
    
    * lint
    
    * update some test cases
    
    * make single SLN tool entry point

---
+++ ui/src/reducers/StarLogoNova/index.js
@@ -0,0 +1,51 @@
+// sln reducer, to save new StarLogoNova models
+import _ from 'lodash'
+
+import {
+  RECEIVE_CREATE_LOCKED_MODEL,
+  CREATE_LOCKED_MODEL_OPTIMISTIC
+} from './createLockedModel'
+
+import { RECEIVE_UPDATE_MODEL_FORM } from './updateModelForm'
+// ------------------------------------
+// Reducer
+// ------------------------------------
+const initialState = {
+  form: {
+    title: null,
+    description: null,
+    projectStr: null
+  },
+  saving: false
+}
+export default function slnReducer (state = initialState, action) {
+  switch (action.type) {
+    case CREATE_LOCKED_MODEL_OPTIMISTIC:
+      return {
+        form: {
+          title: null,
+          description: null,
+          projectStr: null
+        },
+        saving: true
+      }
+
+    case RECEIVE_CREATE_LOCKED_MODEL:
+      return {
+        form: {
+          title: null,
+          description: null,
+          projectStr: null
+        },
+        saving: false
+      }
+
+    case RECEIVE_UPDATE_MODEL_FORM:
+      return _.assign({}, state, {
+        form: action.data
+      })
+
+    default:
+      return state
+  }
+}
